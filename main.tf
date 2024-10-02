a69d28ee1301ac2a3d10bcaf98341c0ea3e7b9e2
Daniele Trifirò <dtrifiro@redhat.com>
2024-10-02 18:03:56 +0200

    add ci user and bucket policy

--- main.tf
+++ main.tf
@@ -45,3 +45,45 @@ resource "aws_s3_bucket_acl" "sccache" {
   bucket = aws_s3_bucket.sccache.id
   acl    = "public-read"
 }
+
+
+resource "aws_iam_user" "ci_user" {
+  name = "${var.bucket_name}-ci-user"
+
+  # tags = local.tags # I don't have the permissions to tag a user
+}
+
+resource "aws_s3_bucket_policy" "sccache" {
+  bucket = aws_s3_bucket.sccache.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.sccache.arn}/*"
+      },
+      {
+        Sid    = "CustomWriteAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_iam_user.ci_user.arn
+        }
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl",
+        ]
+        Resource = "${aws_s3_bucket.sccache.arn}/*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "credentials" {
+  user = aws_iam_user.ci_user.name
+}
+
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "bucket_name" {
   value       = aws_s3_bucket.sccache.id
   description = "id of the sccache bucket"
 }
+
+output "secret_key" {
+  value     = aws_iam_access_key.credentials.secret
+  sensitive = true
+}
+
+output "access_key" {
+  value     = aws_iam_access_key.credentials.id
+  sensitive = true
+}
